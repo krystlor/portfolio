efde198545d202b6c05b0d2907861d5baf4a9b90
quazkl <krystian.lorych@gmail.com>
2022-11-07 21:26:38 +0100

    origin

--- src/components/FormatWord.tsx
+++ src/components/FormatWord.tsx
@@ -1,27 +1,14 @@
 import React from "react";
-import PropTypes, { InferProps } from "prop-types";
 import { Email } from "react-obfuscate-email";
 
-const FormatWordPropTypes = {
-  word: PropTypes.string.isRequired,
-};
-
-type FormatWordTypes = InferProps<typeof FormatWordPropTypes>;
-
-const FormatWord = ({ word }: FormatWordTypes) => {
+const FormatWord = ({ word = "" }: { word: string }) => {
   let isEmailAddress =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  let isPDFFile = /^.+\.pdf$/; // ends with ".pdf"
+  let isPDFFile = /^.+\.pdf$/;
 
-  if (isEmailAddress.test(word)) {
-    return <Email email={word}>{word}</Email>;
-  } else if (isPDFFile.test(word)) {
-    return <a href={`./files/${word}`}>{word}</a>;
-  } else {
-    return <>{word}</>;
-  }
+  if (isEmailAddress.test(word)) return <Email email={word}>{word}</Email>;
+  if (isPDFFile.test(word)) return <a href={`./files/${word}`}>{word}</a>;
+  return <>{word}</>;
 };
-
-FormatWord.propTypes = FormatWordPropTypes;
 
 export default FormatWord;
---
+++ src/tests/FormatWord.test.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Email } from "react-obfuscate-email";
+import FormatWord from "../components/FormatWord";
+
+describe("Should format email as a proper HTML email component", () => {
+  let word = "example@example.com";
+  test("example@example.com is formatted correctly", () => {
+    expect(FormatWord({ word })).toMatchObject(
+      <Email email={word}>{word}</Email>
+    );
+  });
+});
+describe("Should format PDF file as a proper HTML link code", () => {
+  let word = "example.pdf";
+  test("example.pdf is formatted correctly", () => {
+    expect(FormatWord({ word })).toMatchObject(
+      <a href={`./files/${word}`}>{word}</a>
+    );
+  });
+});
+describe("Should return any other word (except for an email or a pdf file) as it is", () => {
+  let word = "example";
+  test("other words (except for an email or a pdf file) are formatted correctly", () => {
+    expect(JSON.stringify(FormatWord({ word }))).toMatch(word);
+  });
+});
