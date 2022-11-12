55ba6ab9d7566a7344ffbdab48cfd615b336ec4d
quazkl <krystian.lorych@gmail.com>
2022-11-12 17:56:28 +0100

    ncu

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes, { InferProps } from "prop-types";
 import { Container, Row, Col } from "react-bootstrap";
-import { ExternalLink } from "./GithubLink";
+import { ExternalLink } from "./ExternalLink";
 import { ProjectImage } from "./ProjectImage";
 
 const ProjectPropTypes = {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,16 @@ import * as React from "react";
 import "../assets/bootstrap.min.css";
 import "../assets/global.scss";
 import { HelmetSection } from "../components/HelmetSection";
-import { GithubLink } from "../components/GithubLink";
+import { ExternalLink } from "../components/ExternalLink";
 import { BasicInfos } from "../components/BasicInfos";
 import { ProjectsList } from "../components/ProjectsList";
 
 const IndexPage = () => (
   <>
     <HelmetSection />
-    <GithubLink />
+    <ExternalLink href={`${process.env.GITHUB_HREF}`}>
+      {`${process.env.GITHUB_LINKNAME}`}
+    </ExternalLink>
     <h1>{process.env.PAGE_TITLE}</h1>
     <BasicInfos />
     <h2>{process.env.SECTION_TITLE}</h2>
